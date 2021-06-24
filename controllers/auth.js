308d6da130689867a55e642a92d3abce7fd0b9c5
Mauricio Arenas <marenaso@gmail.com>
2021-06-24 12:18:40 -0400

    Generate jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const {request, response} = require("express")
 const bcryptjs = require("bcryptjs")
 const User = require("../models/user")
+const {generateJWT} = require("../helpers/jwt")
 
 const login = async (req = request, res = response) => {
 	try {
@@ -8,22 +9,23 @@ const login = async (req = request, res = response) => {
 
 		// Verify email exist
 		const user = await User.findOne({email})
-		if(!user){
+		if (!user) {
 			return res.status(400).json({msg: "Email/Password invalid - email"})
 		}
 		// Is active user
-		if(!user.status){
+		if (!user.status) {
 			return res.status(400).json({msg: "Email/Password invalid - deactivated"})
 		}
 		// Validate password
 		const isValidPassword = bcryptjs.compareSync(password, user.password)
-		if (!isValidPassword){
+		if (!isValidPassword) {
 			return res.status(400).json({msg: "Email/Password invalid - password"})
 		}
 
 		// Generate JWT
+		const token = await generateJWT(user.id)
 
-		res.json({msg: "is OK"})
+		res.json({user, token})
 	} catch (e) {
 		console.log(e)
 		return res.status(500).json({msg: "Internal server error"})
---
+++ helpers/jwt.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken")
+
+const generateJWT = (uid = "") => {
+	return new Promise(((resolve, reject) => {
+		const payload = {uid}
+		jwt.sign(payload, process.env.JWT_KEY, {
+			expiresIn: "4h"
+		}, (err, token) => {
+			if (err) {
+				console.log(err)
+				reject("Cannot generate token")
+			} else {
+				resolve(token)
+			}
+		})
+	}))
+}
+
+module.exports = {
+	generateJWT
+}
